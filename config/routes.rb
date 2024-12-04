f95111545b7fd251a266e509f990b28c76d328c6
Tim Mecklem <timothy@mecklem.com>
2024-12-04 09:41:10 -0500

    Namespace admin routes for batch actions correctly

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,20 @@ Rails.application.routes.draw do
     ::Administrate::ApplicationController.descendants
   end
 
-  controllers_for(nil).each do |controller|
-    controller
-      .instance_methods
-      .select { |m| m[/.+_batch_action$/] }
-      .each do |method_name|
-        resource = controller.name.demodulize.underscore.delete_suffix('_controller')
+  namespace :admin do
+    controllers_for(nil).each do |controller|
+      controller
+        .instance_methods
+        .select { |m| m[/.+_batch_action$/] }
+        .each do |method_name|
+          resource = controller.name.demodulize.underscore.delete_suffix('_controller')
 
-        resources resource.to_sym do
-          post method_name.to_sym,
-            on: :collection,
-            controller: controller.name.underscore.delete_suffix('_controller')
-        end
+          resources resource.to_sym do
+            post method_name.to_sym,
+              on: :collection,
+              controller: controller.name.underscore.delete_suffix('_controller')
+          end
+      end
     end
   end
 end
